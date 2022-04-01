a40af2bac3c4046e47d4f4bb21fc0a8671fe46e7
triplem <manumathias2012@gmail.com>
2022-04-01 11:56:19 -0300

    Made saparete functions for ipv4/6

--- ip-obs.py
+++ ip-obs.py
@@ -3,52 +3,63 @@
 
 import sys
 
-def ip_encode(bfile,v):
-	
-	file_parsed = [bfile[i:i+v] for i in range(0,len(bfile),v)] # initial byte string into 4/16 bytes (ipv4/6  adress lengh)
-	ips=[]
+def ipv4_encode(bfile):
+
+	b = 4
+	file_parsed = [bfile[i:i+b] for i in range(0,len(bfile),b)] # initial byte string into 4 bytes (ipv4 adress lengh)
+	ips = []
 	
 	for i in file_parsed:
 	
 		ip = ''
-		if (v == 4): #ipv4
+		ip_list = [i[o] for o in range(0,len(i))] # adress groups (1byte)
 
-			ip_list = [i[o] for o in range(0,len(i))] # adress groups (1byte)
+		if (len(bfile) % b): #padd ip up with 0 if necessary
+			while (len(ip_list) < b):
+				ip_list.append(0)
 
-			if (len(bfile) % v): #padd ip up with 0 if necessary
-				while (len(ip_list) < v):
-					ip_list.append(0)
+		for j in range(4):
 
-			for j in range(4):
-				a = str(ip_list[j])
-				ip  += f'{a}'
-				if j < 3:
-					ip += "."			
+			a = str(ip_list[j])
+			ip  += f'{a}'
+			if j < 3:
+				ip += "."			
+	
+		ips.append(ip)
+
+	return ips
+
+def ipv6_encode(bfile):
+
+	b = 16 #bytes
+	file_parsed = [bfile[i:i+b] for i in range(0,len(bfile),b)] # initial byte string into 16 bytes (ipv6  adress lengh)
+	ips = []
+
+	for i in file_parsed:
+	
+		ip = ''
+		ip6_list = [i[o:o+2] for o in range(0,len(i),2)] #  adress group (2bytes)			
 		
-		elif (v == 16): #ipv6
+		if (len(bfile) % b): #padd ip with 0 if necessary
 			
-			ip6_list = [i[o:o+2] for o in range(0,len(i),2)] #  adress group (2bytes)			
-		
-			if (len(bfile) % v): #padd ip with 0 if necessary
-				
-				while len(ip6_list[-1]) < 2:
-					ip6_list[-1] += b'\x00'
-				while (len(ip6_list) < v):
-					ip6_list.append(b'\x00\x00')
+			while len(ip6_list[-1]) < 2:
+				ip6_list[-1] += b'\x00'
+			while (len(ip6_list) < b):
+				ip6_list.append(b'\x00\x00')
 
 			for j in range(8):
 				ip += ip6_list[j].hex()
 				if j < 7:
 					ip += ':'
-		
+
 		ips.append(ip)
 
 	return ips
-
+		
 def main():
 	v = {
-	'ipv4': 4,
-	'ipv6': 16
+	'ipv4': ipv4_encode,
+	'ipv6': ipv6_encode
 	}
 
 	try:
@@ -56,7 +67,7 @@ def main():
 		path = sys.argv[1]
 		method = sys.argv[2]
 		bfile = open(path,'rb').read()
-		print(ip_encode(bfile,v[method]))	
+		print(v[method](bfile))	
 
 	except IndexError:
 
